b4175f2d52652af6e8fa28b18f608ca70a9047c8
Nichole B <nichole.bouffard@mail.utoronto.ca>
2019-12-09 18:53:50 -0500

    more edits and added instruct screen

--- Step2_LoopIt.py
+++ Step2_LoopIt.py
@@ -31,15 +31,22 @@ sessNum = subgui.data[1]
 
 #%% prepare output
 
-ouputFileName = 'data' + os.sep + 'sub' + subjID + '_sess' + sessNum + '.csv'
-if os.path.isfile(ouputFileName) :
+outputFileName = 'data' + os.sep + 'sub' + subjID + '_sess' + sessNum + '.csv'
+if os.path.isfile(outputFileName) :
     sys.exit("data for this session already exists")
 
-outVars = ['subj', 'trial', 'face', 'object', 'sceneFace', 'color', 'rating', 'rt', 'trialOn','stimOn']    
+outVars = ['subj', 'trial', 'face', 'object', 'sceneFace', 'color', 'rating', 'rt','trialStart', 'trialEnd', 'val']    
 out = pd.DataFrame(columns=outVars)
 
+# create output file now, to append to as the experiment progresses
+out['subj'] = subjID
+out.to_csv(outputFileName,index = False)
+
+# instruction file
+instFile = 'instructions.jpeg'
+
 # open a white full screen window
-win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height') 
+win = visual.Window(fullscr=True, allowGUI=False, color='white', units='height') 
 
 # initialize clocks
 trialClock = core.Clock() 
@@ -55,9 +62,9 @@ myScale = visual.RatingScale(win, low=1, high=7, marker='triangle',
     tickMarks=[1,2,3,4,5,6,7],markerStart=None,markerColor='black',pos = (0,-.6))
 
 # feedback
-bigFeedback = visual.TextStim(win, text='Very Vivid', pos=(0,0), height=.05, color = 'black')
-smallFeedback = visual.TextStim(win, text='Not Vivid', pos=(0,0), height=.05,color = 'black')
-neutFeedback = visual.TextStim(win, text='Kind of Vivid', pos=(0,0), height=.05,color = 'black')
+bigFeedback = visual.TextStim(win, text='Very Vivid', pos=(0,0), height=.05, color = 'red')
+smallFeedback = visual.TextStim(win, text='Not Vivid', pos=(0,0), height=.05,color = 'red')
+neutFeedback = visual.TextStim(win, text='Kind of Vivid', pos=(0,0), height=.05,color = 'red')
 
 # randomize trials
 trialInfo = trialInfo.sample(frac=1)
@@ -65,11 +72,23 @@ trialInfo = trialInfo.reset_index()
 
 # set stimulus times in seconds
 isiDur = 1
-stimDur = 3
-fbDur = 3
+stimDur = 5
+fbDur = 1.5
 
 # set number of trials
 nTrials = len(trialInfo)
+
+# prepare image for display
+instr = visual.ImageStim(win, image="instructions.jpg", size = .9)
+
+# draw image to window buffer
+instr.draw()
+# flip window to reveal image
+event.clearEvents() # this clears any prior button presses
+win.flip()
+
+# don't do anything until a key is pressed
+event.waitKeys()
 
 # make your loop
 for t in np.arange(0,nTrials) :
@@ -87,6 +106,7 @@ for t in np.arange(0,nTrials) :
     myText.draw()
 
     # record trial prarameters
+    out[t,'subj'] = subjID
     out.loc[t,'object'] = trialInfo.loc[t,'object']
     out.loc[t,'color'] = trialInfo.loc[t,'color']
     out.loc[t,'face'] = trialInfo.loc[t,'face']
@@ -100,9 +120,9 @@ for t in np.arange(0,nTrials) :
     # then flip your window
     win.flip()
     stimClock.reset()
-    # record when stimulus was prsented and when trial started
-    out.loc[t, 'stimOn'] = expClock.getTime()
-    out.loc[t, 'trialOn'] = trialClock.getTime()
+    # record when stimulus was prsented 
+    out.loc[t, 'trialStart'] = expClock.getTime()
+
     
     myScale.reset()  #reset rating scale right before drawing for RT
     while myScale.noResponse and stimClock.getTime()<stimDur:
@@ -112,16 +132,24 @@ for t in np.arange(0,nTrials) :
             myText.draw()
             win.flip()
     
+     # record when stimulus removed
+    if myScale.noResponse == False: 
+        out.loc[t,'trialEnd'] = expClock.getTime()
+     
+    
     trialResp = myScale.getRating()
     trialRT = myScale.getRT()
     
         # show feedback
     if trialResp < 4:
         smallFeedback.draw()
+        out.loc[t,'val'] = 'notVivid'
     elif trialResp > 4:
         bigFeedback.draw()
+        out.loc[t,'val'] = 'Vivid'
     else:
         neutFeedback.draw()
+        out.loc[t,'val'] = 'Neutral'
     win.flip()
     fbClock.reset()
     while fbClock.getTime() < fbDur:
@@ -130,10 +158,10 @@ for t in np.arange(0,nTrials) :
     
     # save responses       
     if myScale.noResponse == False: #if response was made
-       # out.loc[t, 'rating'] = myScale.getRating()
-       # out.loc[t, 'rt'] = myScale.getRT() 
          out.loc[t, 'rating'] = trialResp
          out.loc[t, 'rt'] = trialRT
+         out.loc[[t]].to_csv(outputFileName, mode = 'a', header = False, index = False)
+      
 
 # finish experiment
 win.flip() 
@@ -150,8 +178,8 @@ win.flip()
 # closes properly
 
 # manage output    
-out['subj'] = subjID
-out.to_csv(ouputFileName, index = False)
+#out['subj'] = subjID
+#out.to_csv(outputFileName, index = False)
 
 
 core.wait(4)
